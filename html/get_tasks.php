5e413b65a39b94642772c37e8e2a44bc42c5642c
qwaq qwaq <qwaq.dev@gmail.com>
2023-12-12 10:40:23 +0500

    Тестик

--- html/get_tasks.php
+++ html/get_tasks.php
@@ -4,7 +4,7 @@ require_once "../databaseconnect.php";
 // Получаем DifficultyID из параметра запроса
 $difficultyID = isset($_GET['difficultyID']) ? intval($_GET['difficultyID']) : 1;
 
-$sql = "SELECT ID, Title FROM tasks WHERE DifficultyID = $difficultyID";
+$sql = "SELECT ID, Title FROM Tasks WHERE DifficultyID = $difficultyID";
 $result = mysqli_query($conn, $sql);
 
 $options = [["Title" => "Не выбрано"]];
