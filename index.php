3497d35a7c3adf178bff4624663443c23b90be3d
qwaq qwaq <qwaq.dev@gmail.com>
2023-12-12 18:39:37 +0500

    сука

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/font.css">
+    <link rel="stylesheet" href="../css/font.css?v.2">
     <link rel="stylesheet" href="../css/lobby.css?v.12">
     <title>Lobby</title>
 </head>
